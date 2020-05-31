4d63eeafdb6835f41be05608393cbdefbc1618f9
Alex Galvis <alex.galvis.sistemas@gmail.com>
2020-05-30 23:02:50 -0500

    Init repo

---
+++ src/main/java/co/com/gsdd/constants/PrintConstants.java
@@ -0,0 +1,18 @@
+package co.com.gsdd.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * 
+ * @author Great System Development Dynamic (<b>GSDD</b>) <br>
+ *         Alexander Galvis Grisales <br>
+ *         alex.galvis.sistemas@gmail.com <br>
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class PrintConstants {
+
+    public static final String NO_PRINT_SERVICE = "Could not find any print service.";
+    public static final String REDIRECTED = "redireccionado";
+
+}
